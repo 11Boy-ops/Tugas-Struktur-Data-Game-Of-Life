c5ae408addada5341e3b3506a2089a4d933136d4
Wahyu Adi Pratama <wahyuardat723@gmail.com>
2026-02-13 16:56:22 +0700

    Create array.py

---
+++ array.py
@@ -0,0 +1,54 @@
+"""
+ADT ARRAY - Sesuai definisi di dokumen
+Array satu dimensi, ukuran tetap, akses via indeks
+"""
+
+class Array:
+    def __init__(self, size):
+        """Array(size): Membuat array dengan ukuran tetap"""
+        if size <= 0:
+            raise ValueError("Ukuran array harus > 0")
+        self._size = size
+        self._data = [None] * size
+    
+    def length(self):
+        """length(): Mengembalikan panjang array"""
+        return self._size
+    
+    def getitem(self, index):
+        """getitem(index): Mengambil nilai di indeks tertentu"""
+        if index < 0 or index >= self._size:
+            raise IndexError("Indeks di luar jangkauan")
+        return self._data[index]
+    
+    def setitem(self, index, value):
+        """setitem(index, value): Mengubah nilai di indeks tertentu"""
+        if index < 0 or index >= self._size:
+            raise IndexError("Indeks di luar jangkauan")
+        self._data[index] = value
+    
+    def clearing(self, value):
+        """clearing(value): Mengosongkan array dengan nilai tertentu"""
+        for i in range(self._size):
+            self._data[i] = value
+    
+    def __iter__(self):
+        """iterator(): Membuat iterator untuk traversal"""
+        return ArrayIterator(self._data)
+
+
+class ArrayIterator:
+    """Iterator untuk menelusuri array"""
+    def __init__(self, data):
+        self._data = data
+        self._index = 0
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self._index < len(self._data):
+            value = self._data[self._index]
+            self._index += 1
+            return value
+        raise StopIteration
